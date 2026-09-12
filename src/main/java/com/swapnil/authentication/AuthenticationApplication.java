047d8932783ef31cc03895fd313a6380b50c11e7
swapnil singh <swapsingh0502@gmail.com>
2026-09-12 18:14:38 +0530

    Added Dockerfile

--- src/main/java/com/swapnil/authentication/AuthenticationApplication.java
+++ src/main/java/com/swapnil/authentication/AuthenticationApplication.java
@@ -2,19 +2,12 @@ package com.swapnil.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
+
 
 
 @SpringBootApplication
 public class AuthenticationApplication {
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure()
-				.ignoreIfMissing()
-				.load();
-
-		dotenv.entries().forEach(entry ->
-				System.setProperty(entry.getKey(), entry.getValue())
-		);
 		SpringApplication.run(AuthenticationApplication.class, args);
 	}
 
